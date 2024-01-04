3b0138087de8e57a4bd8600fddca7ff67376eff5
Rion Kawaguchi <rion_kawaguchi@mail.toyota.co.jp>
2024-01-04 22:40:53 +0900

    first commit
    Co-authored-by: Rion Kawaguchi <rion_kawaguchi@mail.toyota.co.jp>

---
+++ src/main/kotlin/com/example/renderTest/RenderTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.renderTest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RenderTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<RenderTestApplication>(*args)
+}
